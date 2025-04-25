a8a3a68719d86250b3a86e756a7d2a46481a213b
Rene Silva <rene.scre@gmail.com>
2025-04-24 19:01:40 -0600

    decouple library for reading environment vars implemented

--- backend/lucy_core/settings.py
+++ backend/lucy_core/settings.py
@@ -1,5 +1,5 @@
 import dj_database_url
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -10,12 +10,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=k8*te$esx&j8=n_5fy@!k!kcr=4gz)guv72fu+p235@@w%+!o"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ["lucy-pr3k.onrender.com"]
+DEBUG = config("DEBUG", default=False, cast=bool)
+
+if DEBUG:
+    ALLOWED_HOSTS = ["localhost"]
+else:
+    ALLOWED_HOSTS = ["lucy-pr3k.onrender.com"]
 
 
 # Application definition
