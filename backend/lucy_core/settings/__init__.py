e99594a5cc71a44e9b653824b9512f3f151d7419
Rene Silva <rene.scre@gmail.com>
2025-05-13 21:01:44 -0600

    fixes

--- backend/lucy_core/settings/__init__.py
+++
@@ -1,9 +0,0 @@
-from decouple import config
-
-env_settings = config("DJANGO_ENV", default="dev")
-
-if env_settings == "prod":
-    print("AMBIENTE", env_settings)
-    from .prod import *
-else:
-    from .dev import *
