1f20b3024f7766660a24da6120f52d8417c5bf5e
Rene Silva <rene.scre@gmail.com>
2025-05-22 17:11:50 -0600

    uvicorn configuration finished

--- backend/lucy_core/asgi.py
+++ backend/lucy_core/asgi.py
@@ -1,17 +1,22 @@
 import os
 import django
-from decouple import config
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+setting_module = env("DJANGO_SETTINGS_MODULE", default="lucy_core.settings.dev")
+os.environ["DJANGO_SETTINGS_MODULE"] = setting_module
+
+print("EUREKA", setting_module)
+
+django.setup()
+
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from lucy_core import routing
 from . import urls
-
-
-setting_module = config("DJANGO_SETTINGS_MODULE")
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{setting_module}")
-
 
 application = ProtocolTypeRouter(
     {
