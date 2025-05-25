33ab4d389d2fd17b461efc8235d455ad3801e9b4
Rene Silva <rene.scre@gmail.com>
2025-05-25 14:35:19 -0600

    Make file finished

--- backend/lucy_core/routing.py
+++ backend/lucy_core/routing.py
@@ -5,11 +5,6 @@ from decouple import config
 # from chat.consumers import ChatConsumer
 # from notifications.consumers import NotificationConsumer
 
-if config("AUTH_MIDDLE") == "True":
-    websocket_urlpatterns = [
-        re_path(r"ws/chat/$", ChatConsumerGeneral.as_asgi()),
-    ]
-else:
-    websocket_urlpatterns = [
-        re_path(r"ws/chat/$", ChatConsumerGeneral.as_asgi()),
-    ]
+websocket_urlpatterns = [
+    re_path(r"ws/chat/$", ChatConsumerGeneral.as_asgi()),
+]
