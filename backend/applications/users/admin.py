172463391b9410fb41c831e29ae30f44bcfcd883
Rene Silva <rene.scre@gmail.com>
2025-06-12 12:11:22 -0600

    admin file

--- backend/applications/users/admin.py
+++ backend/applications/users/admin.py
@@ -1,6 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from applications.users.models.users import User
 from applications.users.models.user_profile import UserProfile
 
 
-admin.site.register(User)
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = [
+        "email",
+        "first_name",
+        "last_name",
+        "username",
+        "role",
+        "is_staff",
+        "is_active",
+    ]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "role")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "first_name", "last_name", "role", "password"),
+            },
+        ),
+    )
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+
+    def save_model(self, request, obj, form, change):
+
+        if not obj.password.startswith("pbkdf2_"):
+            obj.set_password(obj.password)
+        super().save_model(request, obj, form, change)
+
+
+# Register your models here.
+admin.site.register(UserProfile)
